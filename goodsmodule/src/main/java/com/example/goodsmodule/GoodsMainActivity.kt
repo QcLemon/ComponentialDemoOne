79c3eeba83ff489c3d9460dc798edc97296801bc
QiuLemon <747207156@qq.com>
2023-06-14 15:24:43 +0800

    添加组件化依赖

---
+++ goodsmodule/src/main/java/com/example/goodsmodule/GoodsMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.goodsmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoodsMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.goods_activity_goods_main)
+    }
+}
